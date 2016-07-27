3ee29658e691ab7d2cee077ab37c1865d4a26dc9
Elena <lena@realrussia.co.uk>
2016-07-27 14:13:26 +0300

    Height: auto, animate

--- accordion.js
+++ accordion.js
@@ -1,48 +1,52 @@
 "use strict";
 
 var head, //".accordion > h3"
-	content; //".accordion > div"
+	content, //".accordion > div"
+	animateTime = 500;
 
 //при клике по заголовку...
 $(".accordion > h3").click(function () {
 
-	//Перебрать элементы массива (h3)
+	head = $(this);
+	content = $(this).next("div");
+
+	//Перебрать элементы массива (все h3)
 	$(".accordion > h3").each(function (index) {
 		let elem = $(this);
 
 		//Проверить, есть ли уже растрытые блоки. Если есть - закрыть.
 		if (elem.attr("class") === "opened") {
 			elem.removeClass("opened");
-			elem.next("div").animate({
-				height: 0
-			}, 500);
+			elem.next("div").stop().animate({
+				height: '0'
+			}, animateTime);
 		}
 	});
-
-	head = $(this);
-	content = $(this).next("div");
 
 	//Добавить заголовку раскрываемого блока класс "opened"
 	head.addClass("opened");
 
-	//Высчитать высоту раскрываемого блока
-	var thisHeight = 0;
-	content.children().each(function () {
-		thisHeight += $(this).height();
-	});
-
-	//Плавно раскрыть блок
-	content.animate({
-		height: thisHeight + 50
-
-	}, 500);
+	//Плавно раскрыть блок с контентом
+	autoHeightAnimate(content, animateTime);
 
 	//Прокрутить страницу до раскрытого заголовка
 	//это нужно, чтобы контент не уплывал вверх, особенно на маленьких экранах и при большом объеме контента
 	setTimeout(function () {
 		$("html, body").animate({
 			scrollTop: head.offset().top
-		}, 500);
-	}, 500);
+		}, animateTime);
+	}, animateTime);
+
+	/*****/
+	/* Функция для - animate height: auto */
+	function autoHeightAnimate(element, time) {
+		var curHeight = element.height(), // Получить высоту по умолчанию
+			autoHeight = element.css('height', 'auto').height(); // Сделать высоту auto
+
+		element.height(curHeight); //Переустановить высоту
+		element.stop().animate({
+			height: autoHeight
+		}, time);
+	}
 
 });
